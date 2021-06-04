d4982542b9a395751da6f7c1222aa7f4e2fd79b9
Alex Reinking <alex.reinking@gmail.com>
2021-06-04 14:24:36 -0700

    Test both glob and non-glob and take diff

--- test-glob.py
+++ test-glob.py
@@ -17,24 +17,16 @@ def chdir(path):
         os.chdir(prev)
 
 
-def create_testcase(source, *, n=1024, depth=1):
+def create_testcase(source, *, n=1024, depth=1, use_globs=True):
     assert depth == 1, "Not implemented"
 
     source.mkdir()
     with chdir(source):
-        Path('CMakeLists.txt').write_text(dedent(
-            '''
-            cmake_minimum_required(VERSION 3.20)
-            project(glob-test)
-    
-            file(GLOB_RECURSE sources CONFIGURE_DEPENDS "src/**.cpp")
-            add_executable(main ${sources})
-            '''
-        ).lstrip())
-
         Path('src').mkdir()
 
-        Path('src/main.cpp').write_text(dedent(
+        sources = [Path('src/main.cpp')]
+
+        sources[0].write_text(dedent(
             '''
             int main () {
                 return 0;
@@ -43,7 +35,25 @@ def create_testcase(source, *, n=1024, depth=1):
         ).lstrip())
 
         for i in range(1, n):
-            Path(f'src/src{i}.cpp').touch()
+            p = Path(f'src/src{i}.cpp')
+            p.touch()
+            sources.append(p)
+
+        if use_globs:
+            sources_list = 'file(GLOB_RECURSE sources CONFIGURE_DEPENDS "src/**.cpp")'
+        else:
+            sources_list = " ".join(map(Path.as_posix, sources))
+            sources_list = f'set(sources {sources_list})'
+
+        Path('CMakeLists.txt').write_text(dedent(
+            f'''
+            cmake_minimum_required(VERSION 3.20)
+            project(glob-test)
+
+            {sources_list}
+            add_executable(main ${{sources}})
+            '''
+        ).lstrip())
 
 
 def time_command(cmd):
@@ -58,7 +68,7 @@ def is_single_config(generator):
             and 'Multi' not in generator)
 
 
-def test_overhead(source, build, generator='Ninja'):
+def test_overhead(source, build, generator):
     if is_single_config(generator):
         os.system(f'cmake -G "{generator}" -S {source} -B {build} -DCMAKE_BUILD_TYPE=Release')
         build_cmd = f'cmake --build {build}'
@@ -69,22 +79,33 @@ def test_overhead(source, build, generator='Ninja'):
     return time_command(build_cmd)
 
 
-def main():
-    source = Path('./test')
+def run_test(source, build, generator, *, test_args):
     if source.exists():
         shutil.rmtree(source)
-
-    build = Path('./build')
     if build.exists():
         shutil.rmtree(build)
 
-    default_generator = ('Visual Studio 16 2019' if os.name == 'nt'
-                         else 'Ninja')
+    create_testcase(source, **test_args)
+    return test_overhead(source, build, generator)
 
-    create_testcase(source, n=1024)
-    overhead = test_overhead(source, build, default_generator)
 
-    print(f'Overhead was: {overhead} seconds')
+def main():
+    generator = ('Visual Studio 16 2019' if os.name == 'nt' else 'Ninja')
+
+    source = Path('./test')
+    build = Path('./build')
+
+    no_op_glob = run_test(source, build, generator, test_args=dict(
+        n=1024,
+        use_globs=True
+    ))
+
+    no_op_explicit = run_test(source, build, generator, test_args=dict(
+        n=1024,
+        use_globs=False
+    ))
+    
+    print(f'Overhead was: {no_op_glob - no_op_explicit} seconds')
 
 
 if __name__ == '__main__':
