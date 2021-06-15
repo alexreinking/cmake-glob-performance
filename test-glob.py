11baef326267c01d5f17df741646257fce55ad5e
Alex Reinking <alex.reinking@gmail.com>
2021-06-15 05:33:50 -0700

    Add argument parsing

--- test-glob.py
+++ test-glob.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+import argparse
 import contextlib
 import os
 import shutil
 import subprocess
 import time
+from multiprocessing import cpu_count
 from pathlib import Path
 from textwrap import dedent
 
@@ -71,10 +73,10 @@ def is_single_config(generator):
 def test_overhead(source, build, generator):
     if is_single_config(generator):
         os.system(f'cmake -G "{generator}" -S {source} -B {build} -DCMAKE_BUILD_TYPE=Release')
-        build_cmd = f'cmake --build {build}'
+        build_cmd = f'cmake --build {build} -j {cpu_count()}'
     else:
         os.system(f'cmake -G "{generator}" -S {source} -B {build}')
-        build_cmd = f'cmake --build {build} --config Release'
+        build_cmd = f'cmake --build {build} --config Release -j {cpu_count()}'
     os.system(build_cmd)
     return time_command(build_cmd)
 
@@ -90,22 +92,44 @@ def run_test(source, build, generator, *, test_args):
 
 
 def main():
-    generator = ('Visual Studio 16 2019' if os.name == 'nt' else 'Ninja')
-
-    source = Path('./test')
-    build = Path('./build')
-
-    no_op_glob = run_test(source, build, generator, test_args=dict(
-        n=1024,
-        use_globs=True
+    default_generator = os.getenv('CMAKE_GENERATOR', default=(
+        'Visual Studio 16 2019' if os.name == 'nt' else 'Ninja'
     ))
 
-    no_op_explicit = run_test(source, build, generator, test_args=dict(
-        n=1024,
-        use_globs=False
-    ))
+    parser = argparse.ArgumentParser(description='Test CMake glob performance')
+    parser.add_argument('--generator', type=str, default=default_generator,
+                        metavar='GEN',
+                        help='the CMake generator to use (e.g. Ninja)')
+    parser.add_argument('--source', type=Path, default=Path('./test'),
+                        help='directory in which to place the test case')
+    parser.add_argument('--build', type=Path, default=Path('./build'),
+                        help='directory in which to build the test case')
+    parser.add_argument('-n', type=int, default=1024,
+                        help='number of files to create')
+    args = parser.parse_args()
+
+
+    no_op_glob = run_test(
+        args.source,
+        args.build,
+        args.generator,
+        test_args=dict(
+            n=args.n,
+            use_globs=True
+        )
+    )
+
+    no_op_explicit = run_test(
+        args.source,
+        args.build,
+        args.generator,
+        test_args=dict(
+            n=args.n,
+            use_globs=False
+        )
+    )
     
-    print(f'Overhead was: {no_op_glob - no_op_explicit} seconds')
+    print(f'Overhead was: {no_op_glob - no_op_explicit:.04f} seconds')
 
 
 if __name__ == '__main__':
